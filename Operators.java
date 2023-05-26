ac3f2f5e5fe6c088e9a9633403d028cad06ad1c4
MSP079 <m.saipraneeth23@gmail.com>
2023-05-26 17:13:50 +0530

    Program in java langauage

---
+++ Operators.java
@@ -0,0 +1,36 @@
+package projectk;
+
+//Java code to illustrate Addition operator
+public class Operators{
+		public static void main(String[] args)
+		{
+		// initializing variables
+		int num1 = 10, num2 = 20, sum = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// adding num1 and num2
+		sum = num1 + num2;
+		System.out.println("The sum = " + sum);
+		
+		
+		// Java code to illustrate Division operator
+			{
+				// initializing variables
+				int a = 12345, b = 1000, div = 0;
+
+				// Displaying num1 and num2
+				System.out.println("num1 = " + a);
+				System.out.println("num2 = " + b);
+
+				// Dividing num1 and num2
+				div = a / b;
+				System.out.println("Division = " + div);
+				
+			}
+	
+
+		}
+}
